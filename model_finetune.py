3976a6ece1f10629318bf1ee619fe0398ca4b8c5
lgq12697 <ljq12697@gmail.com>
2024-10-19 10:30:09 +0800

    Update model_finetune.py

--- model_finetune.py
+++ model_finetune.py
@@ -23,237 +23,241 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification, Auto
                          TrainingArguments, Trainer, HfArgumentParser
 from datasets import Dataset, DatasetDict, load_dataset
 
-from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
-from mamba_ssm.utils.hf import load_config_hf, load_state_dict_hf
-
+try:
+    from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
+    from mamba_ssm.utils.hf import load_config_hf, load_state_dict_hf
+    mamba_available = True
+except:
+    mamba_available = False
+    print('Mamba-ssm is not installed on the machine, cannot run DNA Mamba momdel.')
 
 ############################################################
 ## Arguments ###############################################
+if mamba_available:
+    @dataclass
+    class ModelArguments:
+        model_name_or_path: Optional[str] = field(default="./models/plantdnabert")
+        tokenizer_path: Optional[str] = field(default=None)
+        train_task: Optional[str] = field(default='classification')
+        load_checkpoint: Optional[str] = field(default=None)
+        # select model source
+        source: Optional[str] = field(default="huggingface")
 
-@dataclass
-class ModelArguments:
-    model_name_or_path: Optional[str] = field(default="./models/plantdnabert")
-    tokenizer_path: Optional[str] = field(default=None)
-    train_task: Optional[str] = field(default='classification')
-    load_checkpoint: Optional[str] = field(default=None)
-    # select model source
-    source: Optional[str] = field(default="huggingface")
+    @dataclass
+    class DataArguments:
+        train_data: str = field(default=None, metadata={"help": "Path to the training data."})
+        eval_data: Optional[str] = field(default=None, metadata={"help": "Path to the valid data."})
+        test_data: Optional[str] = field(default=None, metadata={"help": "Path to the test data."})
+        split: float = field(default=0.1, metadata={"help": "Test split"})
+        samples: Optional[int] = field(default=1e8)
+        key: str = field(default='sequence', metadata={"help": "Feature name"})
+        kmer: int = field(default=-1, metadata={"help": "k-mer for DNABERT model"})
+        labels: str = field(default='No;Yes', metadata={"help": "Labels"})
+        shuffle: bool = field(default=False)
 
-@dataclass
-class DataArguments:
-    train_data: str = field(default=None, metadata={"help": "Path to the training data."})
-    eval_data: Optional[str] = field(default=None, metadata={"help": "Path to the valid data."})
-    test_data: Optional[str] = field(default=None, metadata={"help": "Path to the test data."})
-    split: float = field(default=0.1, metadata={"help": "Test split"})
-    samples: Optional[int] = field(default=1e8)
-    key: str = field(default='sequence', metadata={"help": "Feature name"})
-    kmer: int = field(default=-1, metadata={"help": "k-mer for DNABERT model"})
-    labels: str = field(default='No;Yes', metadata={"help": "Labels"})
-    shuffle: bool = field(default=False)
+    @dataclass
+    class TrainingArguments(TrainingArguments):
+        cache_dir: Optional[str] = field(default=None)
+        run_name: str = field(default="runs")
+        optim: str = field(default="adamw_torch")
+        model_max_length: int = field(default=512, metadata={"help": "Maximum sequence length."})
+        gradient_accumulation_steps: int = field(default=1)
+        per_device_train_batch_size: int = field(default=1)
+        per_device_eval_batch_size: int = field(default=1)
+        num_train_epochs: int = field(default=1)
+        fp16: bool = field(default=False)
+        bf16: bool = field(default=False)
+        logging_steps: Optional[int] = field(default=500)
+        logging_strategy: str = field(default='epoch'),
+        save_steps: Optional[int] = field(default=200)
+        save_strategy: str = field(default='epoch'),
+        eval_steps: Optional[int] = field(default=None)
+        evaluation_strategy: str = field(default='epoch'),
+        # eval_accumulation_steps: Optional[int] = field(default=None),
+        warmup_steps: int = field(default=50)
+        weight_decay: float = field(default=0.01)
+        learning_rate: float = field(default=1e-5)
+        save_total_limit: int = field(default=5)
+        load_best_model_at_end: bool = field(default=True)
+        output_dir: str = field(default="output")
+        find_unused_parameters: bool = field(default=False)
+        checkpointing: bool = field(default=False)
+        dataloader_pin_memory: bool = field(default=False)
+        seed: int = field(default=7)
 
-@dataclass
-class TrainingArguments(TrainingArguments):
-    cache_dir: Optional[str] = field(default=None)
-    run_name: str = field(default="runs")
-    optim: str = field(default="adamw_torch")
-    model_max_length: int = field(default=512, metadata={"help": "Maximum sequence length."})
-    gradient_accumulation_steps: int = field(default=1)
-    per_device_train_batch_size: int = field(default=1)
-    per_device_eval_batch_size: int = field(default=1)
-    num_train_epochs: int = field(default=1)
-    fp16: bool = field(default=False)
-    bf16: bool = field(default=False)
-    logging_steps: Optional[int] = field(default=500)
-    logging_strategy: str = field(default='epoch'),
-    save_steps: Optional[int] = field(default=200)
-    save_strategy: str = field(default='epoch'),
-    eval_steps: Optional[int] = field(default=None)
-    evaluation_strategy: str = field(default='epoch'),
-    # eval_accumulation_steps: Optional[int] = field(default=None),
-    warmup_steps: int = field(default=50)
-    weight_decay: float = field(default=0.01)
-    learning_rate: float = field(default=1e-5)
-    save_total_limit: int = field(default=5)
-    load_best_model_at_end: bool = field(default=True)
-    output_dir: str = field(default="output")
-    find_unused_parameters: bool = field(default=False)
-    checkpointing: bool = field(default=False)
-    dataloader_pin_memory: bool = field(default=False)
-    seed: int = field(default=7)
-
-############################################################
-
-
-############################################################
-## Mamba model configurations ##############################
-
-@dataclass
-class MambaConfig:
-    d_model: int = 768
-    n_layer: int = 24
-    vocab_size: int = 8000
-    ssm_cfg: dict = field(default_factory=dict)
-    rms_norm: bool = True
-    residual_in_fp32: bool = True
-    fused_add_norm: bool = True
-    pad_vocab_size_multiple: int = 8
-    tie_embeddings: bool = True
-
-    def to_json_string(self):
-        return json.dumps(asdict(self))
-
-    def to_dict(self):
-        return asdict(self)
+    ############################################################
 
 
-class MambaClassificationHead(nn.Module):
-    def __init__(self, d_model, num_classes, **kwargs):
-        super(MambaClassificationHead, self).__init__()
-        self.classification_head = nn.Linear(d_model, num_classes, **kwargs)
+    ############################################################
+    ## Mamba model configurations ##############################
 
-    def forward(self, hidden_states):
-        return self.classification_head(hidden_states)
+    @dataclass
+    class MambaConfig:
+        d_model: int = 768
+        n_layer: int = 24
+        vocab_size: int = 8000
+        ssm_cfg: dict = field(default_factory=dict)
+        rms_norm: bool = True
+        residual_in_fp32: bool = True
+        fused_add_norm: bool = True
+        pad_vocab_size_multiple: int = 8
+        tie_embeddings: bool = True
 
+        def to_json_string(self):
+            return json.dumps(asdict(self))
 
-class MambaSequenceClassification(MambaLMHeadModel):
-    def __init__(
-        self,
-        config: MambaConfig,
-        initializer_cfg=None,
-        device=None,
-        dtype=None,
-        num_classes=2,
-    ) -> None:
-        super().__init__(config, initializer_cfg, device, dtype)
-
-        self.classification_head = MambaClassificationHead(d_model=config.d_model, num_classes=num_classes)
-
-        del self.lm_head
-
-    def forward(self, input_ids, attention_mask=None, labels=None):
-        hidden_states = self.backbone(input_ids)
-
-        mean_hidden_states = hidden_states.mean(dim=1)
-
-        logits = self.classification_head(mean_hidden_states)
-
-        if labels is None:
-            ClassificationOutput = namedtuple("ClassificationOutput", ["logits"])
-            return ClassificationOutput(logits=logits)
-        else:
-            ClassificationOutput = namedtuple("ClassificationOutput", ["loss", "logits"])
-
-        loss_fct = nn.CrossEntropyLoss()
-        loss = loss_fct(logits, labels)
-
-        return ClassificationOutput(loss=loss, logits=logits)
-
-    def predict(self, text, tokenizer, id2label=None):
-        input_ids = torch.tensor(tokenizer(text)['input_ids'], device='cuda')[None]
-        with torch.no_grad():
-            logits = self.forward(input_ids).logits[0]
-            label = np.argmax(logits.cpu().numpy())
-
-        if id2label is not None:
-            return id2label[label]
-        else:
-            return label
-
-    @classmethod
-    def from_pretrained(cls, pretrained_model_name, device=None, dtype=None, num_classes=2, **kwargs):
-        config_data = load_config_hf(pretrained_model_name)
-        config = MambaConfig(**config_data)
-
-        model = cls(config, device=device, dtype=dtype, num_classes=num_classes, **kwargs)
-
-        model_state_dict = load_state_dict_hf(pretrained_model_name, device=device, dtype=dtype)
-        model.load_state_dict(model_state_dict, strict=False)
-
-        print("Newly initialized embedding:", set(model.state_dict().keys()) - set(model_state_dict.keys()))
-        return model
+        def to_dict(self):
+            return asdict(self)
 
 
-class MambaSequenceRegression(MambaLMHeadModel):
-    def __init__(
-        self,
-        config: MambaConfig,
-        initializer_cfg=None,
-        device=None,
-        dtype=None,
-    ) -> None:
-        super().__init__(config, initializer_cfg, device, dtype)
+    class MambaClassificationHead(nn.Module):
+        def __init__(self, d_model, num_classes, **kwargs):
+            super(MambaClassificationHead, self).__init__()
+            self.classification_head = nn.Linear(d_model, num_classes, **kwargs)
 
-        self.classification_head = MambaClassificationHead(d_model=config.d_model, num_classes=1)
-
-        del self.lm_head
-
-    def forward(self, input_ids, attention_mask=None, labels=None):
-        hidden_states = self.backbone(input_ids)
-
-        mean_hidden_states = hidden_states.mean(dim=1)
-
-        logits = self.classification_head(mean_hidden_states)
-
-        if labels is None:
-            ClassificationOutput = namedtuple("ClassificationOutput", ["logits"])
-            return ClassificationOutput(logits=logits)
-        else:
-            ClassificationOutput = namedtuple("ClassificationOutput", ["loss", "logits"])
-
-        loss_fct = nn.MSELoss()
-        loss = loss_fct(logits.squeeze(), labels.squeeze())
-
-        return ClassificationOutput(loss=loss, logits=logits)
-
-    def predict(self, text, tokenizer, id2label=None):
-        input_ids = torch.tensor(tokenizer(text)['input_ids'], device='cuda')[None]
-        with torch.no_grad():
-            logits = self.forward(input_ids).logits[0]
-            label = logits.cpu().numpy()
-
-        if id2label is not None:
-            return id2label[label]
-        else:
-            return label
-
-    @classmethod
-    def from_pretrained(cls, pretrained_model_name, device=None, dtype=None, **kwargs):
-        config_data = load_config_hf(pretrained_model_name)
-        config = MambaConfig(**config_data)
-
-        model = cls(config, device=device, dtype=dtype, **kwargs)
-
-        model_state_dict = load_state_dict_hf(pretrained_model_name, device=device, dtype=dtype)
-        model.load_state_dict(model_state_dict, strict=False)
-
-        print("Newly initialized embedding:", set(model.state_dict().keys()) - set(model_state_dict.keys()))
-        return model
+        def forward(self, hidden_states):
+            return self.classification_head(hidden_states)
 
 
-class MambaTrainer(Trainer):
+    class MambaSequenceClassification(MambaLMHeadModel):
+        def __init__(
+            self,
+            config: MambaConfig,
+            initializer_cfg=None,
+            device=None,
+            dtype=None,
+            num_classes=2,
+        ) -> None:
+            super().__init__(config, initializer_cfg, device, dtype)
 
-    def compute_loss(self, model, inputs, return_outputs=False):
-        input_ids = inputs.pop("input_ids")
-        labels = inputs.pop('labels')
+            self.classification_head = MambaClassificationHead(d_model=config.d_model, num_classes=num_classes)
 
-        outputs = model(input_ids=input_ids, labels=labels)
-        loss = outputs.loss
+            del self.lm_head
 
-        return (loss, outputs) if return_outputs else loss
+        def forward(self, input_ids, attention_mask=None, labels=None):
+            hidden_states = self.backbone(input_ids)
 
-    def save_model(self, output_dir = None, _internal_call = False):
-        if output_dir is None:
-            output_dir = self.args.output_dir
+            mean_hidden_states = hidden_states.mean(dim=1)
 
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
+            logits = self.classification_head(mean_hidden_states)
 
-        torch.save(self.model.state_dict(), f"{output_dir}/pytorch_model.bin")
+            if labels is None:
+                ClassificationOutput = namedtuple("ClassificationOutput", ["logits"])
+                return ClassificationOutput(logits=logits)
+            else:
+                ClassificationOutput = namedtuple("ClassificationOutput", ["loss", "logits"])
 
-        self.tokenizer.save_pretrained(output_dir)
+            loss_fct = nn.CrossEntropyLoss()
+            loss = loss_fct(logits, labels)
 
-        with open(f'{output_dir}/config.json', 'w') as f:
-            json.dump(self.model.config.to_dict(), f)
+            return ClassificationOutput(loss=loss, logits=logits)
+
+        def predict(self, text, tokenizer, id2label=None):
+            input_ids = torch.tensor(tokenizer(text)['input_ids'], device='cuda')[None]
+            with torch.no_grad():
+                logits = self.forward(input_ids).logits[0]
+                label = np.argmax(logits.cpu().numpy())
+
+            if id2label is not None:
+                return id2label[label]
+            else:
+                return label
+
+        @classmethod
+        def from_pretrained(cls, pretrained_model_name, device=None, dtype=None, num_classes=2, **kwargs):
+            config_data = load_config_hf(pretrained_model_name)
+            config = MambaConfig(**config_data)
+
+            model = cls(config, device=device, dtype=dtype, num_classes=num_classes, **kwargs)
+
+            model_state_dict = load_state_dict_hf(pretrained_model_name, device=device, dtype=dtype)
+            model.load_state_dict(model_state_dict, strict=False)
+
+            print("Newly initialized embedding:", set(model.state_dict().keys()) - set(model_state_dict.keys()))
+            return model
+
+
+    class MambaSequenceRegression(MambaLMHeadModel):
+        def __init__(
+            self,
+            config: MambaConfig,
+            initializer_cfg=None,
+            device=None,
+            dtype=None,
+        ) -> None:
+            super().__init__(config, initializer_cfg, device, dtype)
+
+            self.classification_head = MambaClassificationHead(d_model=config.d_model, num_classes=1)
+
+            del self.lm_head
+
+        def forward(self, input_ids, attention_mask=None, labels=None):
+            hidden_states = self.backbone(input_ids)
+
+            mean_hidden_states = hidden_states.mean(dim=1)
+
+            logits = self.classification_head(mean_hidden_states)
+
+            if labels is None:
+                ClassificationOutput = namedtuple("ClassificationOutput", ["logits"])
+                return ClassificationOutput(logits=logits)
+            else:
+                ClassificationOutput = namedtuple("ClassificationOutput", ["loss", "logits"])
+
+            loss_fct = nn.MSELoss()
+            loss = loss_fct(logits.squeeze(), labels.squeeze())
+
+            return ClassificationOutput(loss=loss, logits=logits)
+
+        def predict(self, text, tokenizer, id2label=None):
+            input_ids = torch.tensor(tokenizer(text)['input_ids'], device='cuda')[None]
+            with torch.no_grad():
+                logits = self.forward(input_ids).logits[0]
+                label = logits.cpu().numpy()
+
+            if id2label is not None:
+                return id2label[label]
+            else:
+                return label
+
+        @classmethod
+        def from_pretrained(cls, pretrained_model_name, device=None, dtype=None, **kwargs):
+            config_data = load_config_hf(pretrained_model_name)
+            config = MambaConfig(**config_data)
+
+            model = cls(config, device=device, dtype=dtype, **kwargs)
+
+            model_state_dict = load_state_dict_hf(pretrained_model_name, device=device, dtype=dtype)
+            model.load_state_dict(model_state_dict, strict=False)
+
+            print("Newly initialized embedding:", set(model.state_dict().keys()) - set(model_state_dict.keys()))
+            return model
+
+
+    class MambaTrainer(Trainer):
+
+        def compute_loss(self, model, inputs, return_outputs=False):
+            input_ids = inputs.pop("input_ids")
+            labels = inputs.pop('labels')
+
+            outputs = model(input_ids=input_ids, labels=labels)
+            loss = outputs.loss
+
+            return (loss, outputs) if return_outputs else loss
+
+        def save_model(self, output_dir = None, _internal_call = False):
+            if output_dir is None:
+                output_dir = self.args.output_dir
+
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+
+            torch.save(self.model.state_dict(), f"{output_dir}/pytorch_model.bin")
+
+            self.tokenizer.save_pretrained(output_dir)
+
+            with open(f'{output_dir}/config.json', 'w') as f:
+                json.dump(self.model.config.to_dict(), f)
 
 ############################################################
 
