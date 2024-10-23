16936e5e02ee129ddfbc9623564ae283158b4a1d
forrestzhang <forrest_zhang@163.com>
2024-10-23 20:29:15 +0800

    add marimo as example

---
+++ example/01_use_finetune_model_demo.py
@@ -0,0 +1,42 @@
+import marimo
+
+__generated_with = "0.9.12"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def __(__file__):
+    import sys
+    import os
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+    import marimo as mo
+    from pdllib.api import plant_llms_inference
+    return mo, os, plant_llms_inference, sys
+
+
+@app.cell
+def __():
+    model_path = "zhangtaolab/plant-dnabert-BPE-promoter"
+    sequence = "GGGAAAAAGTGAACTCCATTGTTTTTTCACGCTAAGCAGACCACAATTGCTGCTTGGTACGAAAAGAAAACCGAACCCTTTCACCCACGCACAACTCCATCTCCATTAGCATGGACAGAACACCGTAGATTGAACGCGGGAGGCAACAGGCTAAATCGTCCGTTCAGCCAAAACGGAATCATGGGCTGTTTTTCCAGAAGGCTCCGTGTCGTGTGGTTGTGGTCCAAAAACGAAAAAGAAAGAAAAAAGAAAACCCTTCCCAAGACGTGAAGAAAAGCAATGCGATGCTGATGCACGTTA"
+    return model_path, sequence
+
+
+@app.cell
+def __(model_path, plant_llms_inference, sequence):
+    res = plant_llms_inference(model_path, sequence, source='modelscope', device='auto', max_length=512)
+    return (res,)
+
+
+@app.cell
+def __(res):
+    print(res)
+    return
+
+
+@app.cell
+def __():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ example/readme.md
@@ -0,0 +1,25 @@
+# Use marimo
+
+## Use marimo to run the demo
+
+## Install requirements
+```bash
+pip install -r requirements.txt
+```
+
+## Test environment
+```bash
+python 00_env_test.py
+```
+
+## Compare models
+Run compare model
+```bash
+marimo run 02_compare_finetune_model.py
+```
+
+or
+Edit marimo file
+```bash
+python edit 02_compare_finetune_model.py
+```
